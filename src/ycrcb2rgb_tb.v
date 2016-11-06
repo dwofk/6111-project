0717dc77390fa9302bb7ba96bc500bdd2981001f
Diana Wofk <diana96@mit.edu>
2016-11-06 17:52:35 -0500

    Add ycrcb2rgb module and testbench (untested)

---
+++ src/ycrcb2rgb_tb.v
@@ -0,0 +1,48 @@
+module ycrcb2rgb_tb();
+
+  reg clock, reset;
+  reg y, cr, cb;
+  wire r, g, b;
+
+  ycrcb2rgb dut(
+    .clk    (clock),
+    .rst    (reset),
+    .y      (y),
+    .cr     (cr),
+    .cb     (cb),
+    .r      (r),
+    .g      (g),
+    .b      (b)
+    );
+
+  initial begin
+    clock = 1'b1;
+    forever begin
+      #5 clock = ~clock;   // 10ns period
+    end
+  end
+
+  initial begin
+
+    // first stimulus
+    y = 72;
+    cb = 130;
+    cr = 173;
+
+    // global reset
+    reset = 1'b1;
+    #100 reset = 1'b0;
+
+    #30     // wait for output
+
+    // second stimulus
+    y = 98;
+    cb = 184;
+    cr = 93;
+
+    #30     // wait for output
+
+    #100 $stop;
+  end
+
+endmodule
